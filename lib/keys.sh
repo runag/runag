e2ae0562c3bd2a9c09a2545109334a178076cfe2
Stanislav Senotrusov <stan@senotrusov.com>
2021-08-03 23:07:24 +0200

    keys::create-update-or-verify-key-checksums-on-all-mounted-media

--- lib/keys.sh
+++ lib/keys.sh
@@ -18,4 +18,143 @@ keys::mkdir() {
   if [ ! -d "${HOME}/.keys" ]; then
     mkdir -p -m 0700 "${HOME}/.keys" || fail
   fi
-fi
+}
+
+# WARNING! It may leak key metadata if you have non-ubuntu system,
+# sorry, but I dont have time to carefully think about mounting tmpfs right now
+
+keys::check-tmpfs() {
+  if [ -z "${XDG_RUNTIME_DIR}" ]; then
+    fail "Unable to find XDG_RUNTIME_DIR"
+  fi
+
+  if ! df | grep "\s${XDG_RUNTIME_DIR}\$" | grep --quiet "^tmpfs\s"; then
+    fail "Unable to find sutable tmpfs"
+  fi
+}
+
+keys::maybe-create-or-update-checksum-file() {
+  local current="$1"
+  local new="$2"
+  local title="$3"
+
+  local action
+
+  if [ ! -f "${current}" ]; then
+    cat "${new}" || fail
+    echo ""
+    echo "${title}: Do you want to create the checksum file? (y/n)"
+
+    IFS="" read -r action || fail
+
+    if [ "${action}" = y ] || [ "${action}" = Y ]; then
+      mv "${new}" "${current}" || fail
+    else
+      rm "${new}" || fail
+    fi
+
+    sync || fail
+    return 0
+  fi
+
+  if diff --strip-trailing-cr "${current}" "${new}" >/dev/null 2>&1; then
+    rm "${new}" || fail
+    echo "${title}: Checksums are ok"
+    return 0
+  fi
+
+  if command -v git >/dev/null; then
+    git diff --ignore-cr-at-eol --color --unified=6 --no-index "${current}" "${new}" | tee
+  else
+    diff --strip-trailing-cr --context=6 --color "${current}" "${new}"
+  fi
+
+  echo ""
+  echo "${title}: Do you want to update the checksum file? (y/n)"
+
+  IFS="" read -r action || fail
+
+  if [ "${action}" = y ] || [ "${action}" = Y ]; then
+    mv "${new}" "${current}" || fail
+    sync || fail
+  else
+    rm "${new}" || fail
+  fi
+}
+
+keys::create-or-update-checksums()(
+  keys::check-tmpfs || fail
+
+  local current="checksum.txt"
+  local new; new="$(mktemp -p "${XDG_RUNTIME_DIR}")" || fail
+  local title="$1"
+
+  cd "$1" || fail
+  find -type f \( -not -name "${current}" \) -exec openssl dgst -sha3-256 {} \; | sort >"${new}"
+  test "${PIPESTATUS[*]}" = "0 0" || fail
+
+  keys::maybe-create-or-update-checksum-file "${current}" "${new}" "${title}" || fail
+)
+
+keys::verify-checksums()(
+  keys::check-tmpfs || fail
+  
+  local current="checksum.txt"
+  local new; new="$(mktemp -p "${XDG_RUNTIME_DIR}")" || fail
+  local title="$1"
+
+  cd "$1" || fail
+
+  if [ ! -f "${current}" ]; then
+    fail "${title}: Unable to find checksum file ${current}"
+  fi
+
+  find -type f \( -not -name "${current}" \) -exec openssl dgst -sha3-256 {} \; | sort >"${new}"
+  test "${PIPESTATUS[*]}" = "0 0" || fail
+
+  if diff --strip-trailing-cr "${current}" "${new}" >/dev/null 2>&1; then
+    rm "${new}" || fail
+    echo "${title}: Checksums are ok"
+    return 0
+  fi
+
+  if command -v git >/dev/null; then
+    git diff --ignore-cr-at-eol --color --unified=6 --no-index "${current}" "${new}" | tee
+  else
+    diff --strip-trailing-cr --context=6 --color "${current}" "${new}"
+  fi
+
+  rm "${new}" || fail
+  fail "${title}: Checksums are different!"
+)
+
+keys::create-update-or-verify-key-checksums() {
+  local media="$1"
+  local dir
+
+  for dir in "${media}"/*keys* ; do
+    if [ -d "$dir" ]; then
+      keys::create-or-update-checksums "$dir" || fail
+    fi
+  done
+
+  for dir in "${media}"/copies/*/* ; do
+    if [ -d "$dir" ]; then
+      keys::verify-checksums "$dir" || fail
+    fi
+  done
+}
+
+keys::for-all-mounted-media() {
+  local dir
+
+  for dir in /media/"${USER}"/KEYS-* ; do
+    if [ -d "$dir" ]; then
+      "$1" "$dir" || fail
+    fi
+  done
+}
+
+keys::create-update-or-verify-key-checksums-on-all-mounted-media() {
+  keys::for-all-mounted-media keys::create-update-or-verify-key-checksums || fail
+}
