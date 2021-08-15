03dbb16316053f7831b0d9af8227353bc711c79a
Stanislav Senotrusov <stan@senotrusov.com>
2021-08-15 16:45:01 +0200

    todo

--- lib/fs.sh
+++ lib/fs.sh
@@ -14,6 +14,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# TODO: chown/chmod first and then write
+
 file::sudo-write() {
   local dest="$1"
   local mode="${2:-0644}"
