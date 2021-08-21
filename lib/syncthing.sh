03705d749c2dda0c2cbb925d8054e65c908dc7ab
Stanislav Senotrusov <stan@senotrusov.com>
2021-08-21 05:31:37 +0200

    better systemctl

--- lib/syncthing.sh
+++ lib/syncthing.sh
@@ -24,5 +24,5 @@ syncthing::apt::install() {
   apt::add-key-and-source "https://syncthing.net/release-key.txt" "deb https://apt.syncthing.net/ syncthing stable" "syncthing" || fail "Unable to add syncthing apt source"
   apt::update || fail
   apt::install syncthing || fail
-  sudo systemctl enable --now "syncthing@${SUDO_USER}.service" || fail
+  sudo systemctl --now enable "syncthing@${SUDO_USER}.service" || fail
 }
--- lib/tailscale.sh
+++ lib/tailscale.sh
@@ -85,6 +85,7 @@ Persistent=true
 WantedBy=timers.target
 EOF
 
+  sudo systemctl daemon-reload || fail
   sudo systemctl reenable tailscale-issue-2541-workaround.timer || fail
   sudo systemctl start tailscale-issue-2541-workaround.timer || fail
 }
