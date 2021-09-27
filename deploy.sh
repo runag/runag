ece732ff5fc7f33eea14bf5216ab331807784e42
Stanislav Senotrusov <stan@senotrusov.com>
2021-09-27 03:09:11 +0200

    fixes the deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,122 +14,120 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# Script is wrapped inside a function with a random name in hopes that
+# "curl | bash" will not run some unexpected commands if download fails in the middle.
 __xVhMyefCbBnZFUQtwqCs() {
-  # set shell options
+
+# set shell options
+if [ "${SOPKA_VERBOSE:-}" = true ]; then
+  set -o xtrace
+fi
+set -o nounset
+
+# define fail() function
+fail() {
+  local errorColor=""
+  local normalColor=""
+
+  if [ -t 2 ]; then
+    local colorsAmount; colorsAmount="$(tput colors 2>/dev/null)"
+
+    # shellcheck disable=SC2181
+    if [ $? = 0 ] && [ "${colorsAmount}" -ge 2 ]; then
+      errorColor="$(tput setaf 1)"
+      normalColor="$(tput sgr 0)"
+    fi
+  fi
+
+  echo "${errorColor}${1:-"Abnormal termination"}${normalColor}" >&2
+
+  local i endAt=$((${#BASH_LINENO[@]}-1))
+  for ((i=1; i<=endAt; i++)); do
+    echo "  ${errorColor}${BASH_SOURCE[${i}]}:${BASH_LINENO[$((i-1))]}: in \`${FUNCNAME[${i}]}'${normalColor}" >&2
+  done
+
+  exit "${2:-1}"
+}
+
+# shellcheck disable=SC2034
+apt::update() {
+  SOPKA_APT_LAZY_UPDATE_HAPPENED=true
+  sudo apt-get -qq -o Acquire::ForceIPv4=true update || fail
+}
+
+apt::install() {
+  sudo apt-get -qq -y -o Acquire::ForceIPv4=true install "$@" | apt::shush-dpkg
+  test "${PIPESTATUS[*]}" = "0 0" || fail
+}
+
+apt::shush-dpkg() {
   if [ "${SOPKA_VERBOSE:-}" = true ]; then
-    set -o xtrace
-  fi
-  set -o nounset
-
-  # define fail() function
-  fail() {
-    local errorColor=""
-    local normalColor=""
-
-    if [ -t 2 ]; then
-      local colorsAmount; colorsAmount="$(tput colors 2>/dev/null)"
-
-      # shellcheck disable=SC2181
-      if [ $? = 0 ] && [ "${colorsAmount}" -ge 2 ]; then
-        errorColor="$(tput setaf 1)"
-        normalColor="$(tput sgr 0)"
-      fi
+    tee || fail
+  else
+    grep -vE "^Selecting previously unselected package|^\\(Reading database \\.\\.\\.|^Preparing to unpack .* \\.\\.\\.|^Unpacking .* \\.\\.\\.|^Setting up .* \\.\\.\\.|^Processing triggers for .* \\.\\.\\."
+    if [ "$?" -ge "2" ]; then
+      fail
     fi
-
-    echo "${errorColor}${1:-"Abnormal termination"}${normalColor}" >&2
-
-    local i endAt=$((${#BASH_LINENO[@]}-1))
-    for ((i=1; i<=endAt; i++)); do
-      echo "  ${errorColor}${BASH_SOURCE[${i}]}:${BASH_LINENO[$((i-1))]}: in \`${FUNCNAME[${i}]}'${normalColor}" >&2
-    done
-
-    exit "${2:-1}"
-  }
-
-  git::install-git() {
-    if [[ "${OSTYPE}" =~ ^linux ]]; then
-      if ! command -v git >/dev/null; then
-        if command -v apt-get >/dev/null; then
-          apt::update || fail
-          apt::install git || fail
-        else
-          fail "Unable to install git, apt-get not found"
-        fi
-      fi
-    fi
-
-    # on macos that will start git install process
-    git --version >/dev/null || fail
-  }
-
-  # shellcheck disable=SC2034
-  apt::update() {
-    SOPKA_APT_LAZY_UPDATE_HAPPENED=true
-    sudo apt-get -qq -o Acquire::ForceIPv4=true update || fail
-  }
-
-  apt::install() {
-    sudo apt-get -qq -y -o Acquire::ForceIPv4=true install "$@" | apt::shush-dpkg
-    test "${PIPESTATUS[*]}" = "0 0" || fail
-  }
-
-  apt::shush-dpkg() {
-    if [ "${SOPKA_VERBOSE:-}" = true ]; then
-      tee || fail
-    else
-      grep -vE "^Selecting previously unselected package|^\\(Reading database \\.\\.\\.|^Preparing to unpack .* \\.\\.\\.$|^Unpacking .* \\.\\.\\.$|^Setting up .* \\.\\.\\.$|^Processing triggers for .* \\.\\.\\.$"
-      if [ "$?" -ge "2" ]; then
-        fail
-      fi
-    fi
-  }
-
-  git::place-up-to-date-clone() {
-    local url="$1"
-    local dest="$2"
-    local branch="${3:-}"
-
-    if [ -d "${dest}" ]; then
-      local currentUrl; currentUrl="$(git -C "${dest}" config remote.origin.url)" || fail
-
-      if [ "${currentUrl}" != "${url}" ]; then
-        local destFullPath; destFullPath="$(cd "${dest}" >/dev/null 2>&1 && pwd)" || fail
-        local destParentDir; destParentDir="$(dirname "${destFullPath}")" || fail
-        local destDirName; destDirName="$(basename "${destFullPath}")" || fail
-        local packupPath; packupPath="$(mktemp -u "${destParentDir}/${destDirName}-SOPKA-PREVIOUS-CLONE-XXXXXXXXXX")" || fail
-        mv "${destFullPath}" "${packupPath}" || fail
-        git clone --quiet "${url}" "${dest}" || fail
-      fi
-      git -C "${dest}" pull --quiet || fail
-    else
-      git clone --quiet "${url}" "${dest}" || fail
-    fi
-
-    if [ -n "${branch:-}" ]; then
-      git -C "${dest}" checkout --quiet "${branch}" || fail "Unable to checkout ${branch}"
-    fi
-  }
-
-  sopka::add() {
-    local packageName="$1"
-    local dest; dest="$(echo "${packageName}" | tr "/" "-")" || fail
-    git::place-up-to-date-clone "https://github.com/${packageName}.git" "${HOME}/.sopka/sopkafiles/github-${dest}" || fail
-  }
-
-  git::install-git || fail
-
-  git::place-up-to-date-clone "https://github.com/senotrusov/sopka.git" "${HOME}/.sopka" || fail
-
-  if [ -n "${1:-}" ] && [ "$1" != "--" ]; then
-    sopka::add "$1" || fail
-  fi
-
-  if [ -n "${@:2}" ]; then
-    "${HOME}/.sopka/bin/sopka" "${@:2}" || fail
   fi
 }
 
-# Script is wrapped inside a function with a random name in hopes that
-# "curl | bash" will not run some unexpected commands if download fails in the middle.
+git::install-git() {
+  if [[ "${OSTYPE}" =~ ^linux ]]; then
+    if ! command -v git >/dev/null; then
+      if command -v apt-get >/dev/null; then
+        apt::update || fail
+        apt::install git || fail
+      else
+        fail "Unable to install git, apt-get not found"
+      fi
+    fi
+  fi
 
-__xVhMyefCbBnZFUQtwqCs "$@" || return $?
+  # on macos that will start git install process
+  git --version >/dev/null || fail
+}
+
+git::place-up-to-date-clone() {
+  local url="$1"
+  local dest="$2"
+  local branch="${3:-}"
+
+  if [ -d "${dest}" ]; then
+    local currentUrl; currentUrl="$(git -C "${dest}" config remote.origin.url)" || fail
+
+    if [ "${currentUrl}" != "${url}" ]; then
+      local destFullPath; destFullPath="$(cd "${dest}" >/dev/null 2>&1 && pwd)" || fail
+      local destParentDir; destParentDir="$(dirname "${destFullPath}")" || fail
+      local destDirName; destDirName="$(basename "${destFullPath}")" || fail
+      local packupPath; packupPath="$(mktemp -u "${destParentDir}/${destDirName}-SOPKA-PREVIOUS-CLONE-XXXXXXXXXX")" || fail
+      mv "${destFullPath}" "${packupPath}" || fail
+      git clone --quiet "${url}" "${dest}" || fail
+    fi
+    git -C "${dest}" pull --quiet || fail
+  else
+    git clone --quiet "${url}" "${dest}" || fail
+  fi
+
+  if [ -n "${branch:-}" ]; then
+    git -C "${dest}" checkout --quiet "${branch}" || fail "Unable to checkout ${branch}"
+  fi
+}
+
+sopka::add() {
+  local packageId="$1"
+  local dest; dest="$(echo "${packageId}" | tr "/" "-")" || fail
+  git::place-up-to-date-clone "https://github.com/${packageId}.git" "${HOME}/.sopka/sopkafiles/github-${dest}" || fail
+}
+
+
+git::install-git || fail
+
+git::place-up-to-date-clone "https://github.com/senotrusov/sopka.git" "${HOME}/.sopka" || fail
+
+if [ -n "${1:-}" ] && [ "$1" != "--" ]; then
+  sopka::add "$1" || fail
+fi
+
+"${HOME}/.sopka/bin/sopka" "${@:2}" || fail
+
+}; __xVhMyefCbBnZFUQtwqCs "$@" || return $?
