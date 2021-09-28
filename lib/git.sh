b012f3adfcbfee2b8c49227589da15edf076a299
Stanislav Senotrusov <stan@senotrusov.com>
2021-09-28 20:01:43 +0200

    removes --quiet from git calls

--- lib/git.sh
+++ lib/git.sh
@@ -31,15 +31,15 @@ git::place-up-to-date-clone() {
       local destDirName; destDirName="$(basename "${destFullPath}")" || fail
       local packupPath; packupPath="$(mktemp -u "${destParentDir}/${destDirName}-SOPKA-PREVIOUS-CLONE-XXXXXXXXXX")" || fail
       mv "${destFullPath}" "${packupPath}" || fail
-      git clone --quiet "${url}" "${dest}" || fail
+      git clone "${url}" "${dest}" || fail
     fi
-    git -C "${dest}" pull --quiet || fail
+    git -C "${dest}" pull || fail
   else
-    git clone --quiet "${url}" "${dest}" || fail
+    git clone "${url}" "${dest}" || fail
   fi
 
   if [ -n "${branch:-}" ]; then
-    git -C "${dest}" checkout --quiet "${branch}" || fail "Unable to checkout ${branch}"
+    git -C "${dest}" checkout "${branch}" || fail "Unable to checkout ${branch}"
   fi
 }
 
