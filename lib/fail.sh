1f517301ea4c7355a7d11cbb0ffa3e8f982e85c5
Stanislav Senotrusov <stan@senotrusov.com>
2021-10-05 04:52:29 +0200

    softfail

--- lib/fail.sh
+++ lib/fail.sh
@@ -14,13 +14,48 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# foo || fail ["error message" [<error code>]]
+# foo || fail-with <error code>
+#
+# foo || return
+# foo || softfail ["error message" [<error code>]] || return
+#
+# foo || softfail || return <error code>
+# foo || softfail-with <error code> || return
+
 fail() {
+  softfail::internal "$@"
+  exit
+}
+
+fail-with() {
+  softfail::internal "Abnormal termination" "$1"
+  exit
+}
+
+softfail() {
+  softfail::internal "$@"
+}
+
+softfail-with() {
+  softfail::internal "Abnormal termination" "$1"
+}
+
+softfail::internal() {
+  local exitStatus="${2:-0}"
+
   log::error "${1:-"Abnormal termination"}" || echo "Sopka: Unable to log error" >&2
 
+  # making stack trace inside softfail::internal, we dont want to display fail() or softfail() internals in trace
+  # so here we start from i=2 (instead of normal i=1) to skip first line of stack trace
   local i endAt=$((${#BASH_LINENO[@]}-1))
-  for ((i=1; i<=endAt; i++)); do
-    log::error "  ${BASH_SOURCE[${i}]}:${BASH_LINENO[$((i-1))]}: in \`${FUNCNAME[${i}]}'" || echo "Sopka: Unable to log error" >&2
+  for ((i=2; i<=endAt; i++)); do
+    log::error "  ${BASH_SOURCE[${i}]}:${BASH_LINENO[$((i-1))]}: in \`${FUNCNAME[${i}]}'" || echo "Sopka: Unable to log stack trace" >&2
   done
 
-  exit "${2:-1}"
+  if [ -n "${exitStatus##*[!0-9]*}" ] && [ "${exitStatus}" != 0 ]; then
+    return "${exitStatus}"
+  fi
+
+  return 1
 }
