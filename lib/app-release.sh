12e07a2d39111e678bf9985decd0faa4acc5705b
Stanislav Senotrusov <stan@senotrusov.com>
2021-11-04 21:04:26 +0100

    adds app-release,

---
+++ lib/app-release.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env bash
+
+#  Copyright 2012-2021 Stanislav Senotrusov <stan@senotrusov.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# APP_DIR
+# APP_RELEASE
+
+app-release::init() {
+  local appDir="${APP_DIR:?}"
+
+  local mode="${1:-}"
+
+  dir::make-if-not-exists "${appDir}" "${mode}" || softfail || return $?
+  dir::make-if-not-exists "${appDir}/releases" "${mode}" || softfail || return $?
+  dir::make-if-not-exists "${appDir}/repo" "${mode}" || softfail || return $?
+  dir::make-if-not-exists "${appDir}/shared" "${mode}" || softfail || return $?
+
+  git --bare init "${appDir}/repo" || softfail || return $?
+}
+
+app-release::push-local-repo-to-remote() {
+  local gitRemoteUrl="${REMOTE_USER}@${REMOTE_HOST}:${APP_DIR:?}/repo"
+
+  if ! git config remote.production.url >/dev/null; then
+    git remote add production "${gitRemoteUrl}" || softfail || return $?
+  else
+    git config remote.production.url "${gitRemoteUrl}" || softfail || return $?
+  fi
+
+  git push production master || softfail || return $?
+}
+
+app-release::make() {
+  local appDir="${APP_DIR:?}"
+
+  local currentDate; currentDate="$(date "+%Y%m%dT%H%M%SZ")" || softfail || return $?
+
+  ( cd "${appDir}/releases" && mktemp -d "${currentDate}-XXXX" ) || softfail || return $?
+}
+
+app-release::clone() {
+  local appDir="${APP_DIR:?}"
+  local appReleasePath="${appDir}/releases/${APP_RELEASE:?}"
+  
+  if [ ! -d "${appReleasePath}/.git" ]; then
+    git clone "${appDir}/repo" "${appReleasePath}" >/dev/null || softfail || return $?
+  fi
+}
+
+app-release::link-shared-file() {
+  local appDir="${APP_DIR:?}"
+  local appReleasePath="${appDir}/releases/${APP_RELEASE:?}"
+
+  local linkPath="${appReleasePath}/$1"
+  local target="${2:-"${appDir}/shared/${1}"}"
+
+  if [ ! -e "${target}" ]; then
+    mkdir -p "${target}" || softfail || return $?
+  fi
+
+  local linkDirPath; linkDirPath="$(dirname "${linkPath}")" || softfail || return $?
+
+  if [ ! -e "${linkDirPath}" ]; then
+    mkdir -p "${linkDirPath}" || softfail || return $?
+  fi
+
+  if [ -e "${linkPath}" ]; then
+    local backupName; backupName="$(mktemp -d "${appReleasePath}/.sopka-app-release-link-backup-XXXXXXXXXX")" || softfail || return $?
+    mv "${linkPath}" "${backupName}" || softfail || return $?
+  fi
+
+  local targetAbsolutePath; targetAbsolutePath="$(fs::get-absolute-path "${target}")" || softfail || return $?
+
+  ln --symbolic "${targetAbsolutePath}" "${linkPath}" || softfail || return $?
+}
+
+app-release::link-as-current() {
+  local appDir="${APP_DIR:?}"
+
+  local linkName="${appDir}/current"
+
+  if [ -e "${linkName}" ] && [ ! -L "${linkName}" ]; then
+    softfail "Unable to create a link to current release, file exists: ${linkName}"
+    return $?
+  fi
+
+  ln --symbolic --force --no-dereference "releases/${APP_RELEASE:?}" "${appDir}/current" || softfail || return $?
+}
+
+app-release::cleanup() {
+  local appReleasesCollectionPath="${APP_DIR:?}/releases"
+
+  local keepAmount="${1:-10}"
+
+  local release
+  local removeThisRelease
+
+  for release in "${appReleasesCollectionPath:?}"/*; do
+    echo "${release}"
+  done | sort | head "--lines=-${keepAmount}" | \
+  while IFS="" read -r removeThisRelease; do
+    echo "Removing ${removeThisRelease}..."
+    rm -rf "${removeThisRelease:?}" || softfail || return $?
+  done
+
+  if [[ "${PIPESTATUS[*]}" =~ [^0[:space:]] ]]; then
+    softfail || return $?
+  fi
+}
+
+app-release::with-release-remote-dir() {
+  REMOTE_DIR="${APP_DIR:?}/releases/${APP_RELEASE:?}" "$@"
+}
+
+app-release::sync-to-remote() {
+  local appReleasePath="${APP_DIR:?}/releases/${APP_RELEASE:?}"
+
+  local sourcePath="$1"
+  local destPath="${2:-"$1"}"
+
+  rsync::sync-to-remote "${sourcePath}" "${appReleasePath}/${destPath}" || fail
+}
