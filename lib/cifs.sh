4b5ecf34cef9b47c1fb2eab275358e1660030878
Stanislav Senotrusov <stan@senotrusov.com>
2022-07-09 14:35:38 +0200

    reorders arguments in cifs::credentials

--- lib/cifs.sh
+++ lib/cifs.sh
@@ -20,9 +20,9 @@ cifs::credentials::exists() {
 }
 
 cifs::credentials::save() {
-  local cifs_username="$1"
-  local cifs_password="$2"
-  local credentials_file="$3"
+  local cifs_password="$1"
+  local credentials_file="$2"
+  local cifs_username="$3"
   local mode="${4:-"600"}"
   
   printf "username=%s\npassword=%s\n" "${cifs_username}" "${cifs_password}" | file::write "${credentials_file}" "${mode}"
