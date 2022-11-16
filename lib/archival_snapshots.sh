93aae58bd4966eefbd2d6eca6005644ac3421647
Stanislav Senotrusov <stan@senotrusov.com>
2022-11-16 05:10:21 +0100

    archival_snapshots

---
+++ lib/archival_snapshots.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#  Copyright 2012-2022 Stanislav Senotrusov <stan@senotrusov.com>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# :? here is to make sure we don't accidentally cleanup root directory or some other unexpected location
+
+archival_snapshots::create() {
+  local source="${1:?}"
+  local dest="${2:?}"
+  local snapshot_name="${3:-}"
+
+  local current_date; current_date="$(date --utc "+%Y%m%dT%H%M%SZ")" || softfail || return $?
+
+  btrfs subvolume snapshot -r "${source}" "${dest}/${current_date}${parameter:+"-${snapshot_name}"}" || softfail || return $?
+}
+
+archival_snapshots::cleanup() {
+  local snapshots_dir="${1:?}" 
+  local keep_amount="${2:-10}"
+
+  local snapshot_path
+  local remove_this_snapshot
+
+  if [ ! -f "${snapshots_dir:?}"/.this-is-archival-snapshots-directory ]; then
+    softfail "Unable to find archival snapshots directory flag. To indicate that it is safe to perform automatic cleanup please put \".this-is-archival-snapshots-directory\" file to the directory that you sure it is safe to cleanup."
+    return $? # the expression takes a line of its own, because if softfail fails to set non-zero exit status, then we still need to make sure a return from the function will happen here
+  fi
+
+  for snapshot_path in "${snapshots_dir:?}"/*; do # :? here is to make sure we don't accidentally cleanup root directory
+    echo "${snapshot_path:?}"
+  done | sort | head "--lines=-${keep_amount:?}" | \
+  while IFS="" read -r remove_this_snapshot; do
+    echo "Removing ${remove_this_snapshot}..."
+    sudo btrfs subvolume delete "${remove_this_snapshot:?}" || softfail || return $? # :? here is for no reason
+  done
+
+  if [[ "${PIPESTATUS[*]}" =~ [^0[:space:]] ]]; then
+    softfail || return $?
+  fi
+}
