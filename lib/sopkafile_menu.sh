f0ec0d9d9a48d04c7f6d94773303977a9ff756fb
Stanislav Senotrusov <stan@senotrusov.com>
2022-12-02 10:08:52 +0100

    sopkafile_menu

--- lib/sopkafile_menu.sh
+++ lib/sopkafile_menu.sh
@@ -14,39 +14,39 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-sopkafile_menu::necessary() {
+runagfile_menu::necessary() {
   if [ -n "${1:-}" ] && [[ ! "${OSTYPE}" =~ ^"$1" ]]; then
     return 1
   fi
   [ -t 0 ] && [ -t 1 ]
 }
 
-sopkafile_menu::add() {
+runagfile_menu::add() {
   local command_string; printf -v command_string " %q" "$@" || softfail "Unable to produce command string" || return $?
   SOPKAFILE_MENU+=("${command_string:1}")
 }
 
-sopkafile_menu::add_raw() {
+runagfile_menu::add_raw() {
   SOPKAFILE_MENU+=("$1")
 }
 
-sopkafile_menu::add_delimiter() {
+runagfile_menu::add_delimiter() {
   SOPKAFILE_MENU+=("")
 }
 
-sopkafile_menu::add_header() {
+runagfile_menu::add_header() {
   SOPKAFILE_MENU+=("#$1")
 }
 
-sopkafile_menu::add_subheader() {
+runagfile_menu::add_subheader() {
   SOPKAFILE_MENU+=("##$1")
 }
 
-sopkafile_menu::add_note() {
+runagfile_menu::add_note() {
   SOPKAFILE_MENU+=("#/$1")
 }
 
-sopkafile_menu::display() {
+runagfile_menu::display() {
   if [ -z ${SOPKAFILE_MENU:+x} ]; then
     softfail "Menu is empty"
     return $?
@@ -55,24 +55,24 @@ sopkafile_menu::display() {
   softfail_unless_good_code $?
 }
 
-sopkafile_menu::present() {
+runagfile_menu::present() {
   test -n "${SOPKAFILE_MENU:+x}"
 }
 
-sopkafile_menu::clear() {
+runagfile_menu::clear() {
   SOPKAFILE_MENU=()
 }
 
-sopkafile_menu::add_defaults() {
-  sopkafile_menu::add_header "Same menu with certain flags set" || softfail || return $?
+runagfile_menu::add_defaults() {
+  runagfile_menu::add_header "Same menu with certain flags set" || softfail || return $?
 
-  sopkafile_menu::add task::with_update_secrets sopkafile_menu::display || softfail || return $?
-  sopkafile_menu::add task::with_verbose_task sopkafile_menu::display || softfail || return $?
+  runagfile_menu::add task::with_update_secrets runagfile_menu::display || softfail || return $?
+  runagfile_menu::add task::with_verbose_task runagfile_menu::display || softfail || return $?
 
   if [ -d "${HOME}/.sopka" ]; then
-    sopkafile_menu::add_header "Sopka and sopkafiles" || softfail || return $?
+    runagfile_menu::add_header "Sopka and sopkafiles" || softfail || return $?
     
-    sopkafile_menu::add sopka::create_or_update_offline_install || softfail || return $?
-    sopkafile_menu::add sopka::update || softfail || return $?
+    runagfile_menu::add sopka::create_or_update_offline_install || softfail || return $?
+    runagfile_menu::add sopka::update || softfail || return $?
   fi
 }
