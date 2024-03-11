6f856d04d4356b8e50c02ee24280e5b1a13a9a32
Platan Drizzleword <drizzleword@outlook.com>
2024-03-11 03:33:24 +0100

    better systemd::runagfile_menu

--- lib/systemd.sh
+++ lib/systemd.sh
@@ -36,13 +36,10 @@ systemd::write_system_unit() {
 
 systemd::runagfile_menu() {
   local service_name
-  local ssh_call_prefix=()
-  local ssh_call_direct_prefix=()
-  local systemctl_args=()
-  local library_function_arguments=()
-  local with_timer=false
-  local user_services=false
-  local ssh_call=false
+  local ssh_call
+  local ssh_call_prefix
+  local user_services
+  local with_timer
 
   while [ "$#" -gt 0 ]; do
     case $1 in
@@ -52,25 +49,20 @@ systemd::runagfile_menu() {
       ;;
     -w|--ssh-call-with)
       ssh_call=true
-      ssh_call_prefix+=("$2")
-      ssh_call_direct_prefix+=("$2" "--direct")
+      ssh_call_prefix="$2"
       shift; shift
       ;;
     -s|--ssh-call)
       ssh_call=true
-      ssh_call_prefix+=("ssh::call")
-      ssh_call_direct_prefix+=("ssh::call" "--direct")
-      shift; shift
+      ssh_call_prefix="ssh::call"
+      shift
       ;;
     -u|--user)
       user_services=true
-      systemctl_args+=("--user")
-      library_function_arguments+=("--user")
       shift
       ;;
     -t|--with-timer)
       with_timer=true
-      library_function_arguments+=("--with-timer")
       shift
       ;;
     -*)
@@ -84,40 +76,36 @@ systemd::runagfile_menu() {
 
   runagfile_menu::add --header "Actions on ${service_name} services" || softfail || return $?
 
-  runagfile_menu::add "${ssh_call_prefix[@]}" systemctl "${systemctl_args[@]}" --no-block start "${service_name}.service" || softfail || return $?
-  runagfile_menu::add "${ssh_call_prefix[@]}" systemctl "${systemctl_args[@]}" stop "${service_name}.service" || softfail || return $?
+  runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} systemctl ${user_services:+"--user"} --no-block start "${service_name}.service" || softfail || return $?
+  runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} systemctl ${user_services:+"--user"} stop "${service_name}.service" || softfail || return $?
 
-  if [ "${with_timer}" = true ]; then
-    runagfile_menu::add "${ssh_call_prefix[@]}" systemd::disable_timer "${library_function_arguments[@]}" "${service_name}" || softfail || return $?
+  if [ "${with_timer:-}" = true ]; then
+    runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} systemd::disable_timer ${user_services:+"--user"} "${service_name}" || softfail || return $?
   fi
 
-  runagfile_menu::add "${ssh_call_prefix[@]}" systemd::show_status "${library_function_arguments[@]}" "${service_name}" || softfail || return $?
+  runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} systemd::show_status ${user_services:+"--user"} ${with_timer:+"--with-timer"} "${service_name}" || softfail || return $?
 
   # TODO: eventually remove 
-  if [ "${user_services}" = true ]; then
-    local release_codename; release_codename="$("${ssh_call_prefix[@]}" lsb_release --codename --short)" || softfail || return $?
+  if [ "${user_services:-}" = true ]; then
+    local release_codename; release_codename="$(${ssh_call:+"${ssh_call_prefix}"} lsb_release --codename --short)" || softfail || return $?
     if [ "${release_codename}" = focal ]; then
-      if [ "${ssh_call}" = true ]; then
-        ssh_call_prefix+=("--root") # Watch out!
-        ssh_call_direct_prefix+=("--root") # Watch out!
-      fi
-      runagfile_menu::add "${ssh_call_prefix[@]}"        journalctl "_SYSTEMD_USER_UNIT=${service_name}.service" --since today || softfail || return $?
-      runagfile_menu::add "${ssh_call_direct_prefix[@]}" journalctl "_SYSTEMD_USER_UNIT=${service_name}.service" --since today --follow || softfail || return $?
+      runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} ${ssh_call:+"--root"} journalctl "_SYSTEMD_USER_UNIT=${service_name}.service" --since today || softfail || return $?
+      runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} ${ssh_call:+"--root"} ${ssh_call:+"--direct"} journalctl "_SYSTEMD_USER_UNIT=${service_name}.service" --since today --follow || softfail || return $?
       return # Watch out!
     fi
   fi
 
-  runagfile_menu::add "${ssh_call_prefix[@]}"        journalctl "${systemctl_args[@]}" -u "${service_name}.service" --since today || softfail || return $?
-  runagfile_menu::add "${ssh_call_direct_prefix[@]}" journalctl "${systemctl_args[@]}" -u "${service_name}.service" --since today --follow || softfail || return $?
+  runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} journalctl ${user_services:+"--user"} -u "${service_name}.service" --since today || softfail || return $?
+  runagfile_menu::add ${ssh_call:+"${ssh_call_prefix}"} ${ssh_call:+"--direct"} journalctl ${user_services:+"--user"} -u "${service_name}.service" --since today --follow || softfail || return $?
 }
 
 systemd::disable_timer() {
-  local systemctl_args=()
+  local user_services
 
   while [ "$#" -gt 0 ]; do
     case $1 in
     -u|--user)
-      systemctl_args+=("--user")
+      user_services=true
       shift
       ;;
     -*)
@@ -129,18 +117,41 @@ systemd::disable_timer() {
     esac
   done
 
-  systemctl "${systemctl_args[@]}" stop "${1}.timer" || softfail || return $?
-  systemctl "${systemctl_args[@]}" --quiet disable "${1}.timer" || softfail || return $?
+  systemctl ${user_services:+"--user"} stop "${1}.timer" || softfail || return $?
+  systemctl ${user_services:+"--user"} --quiet disable "${1}.timer" || softfail || return $?
+}
+
+systemd::enable_timer() {
+  local user_services
+
+  while [ "$#" -gt 0 ]; do
+    case $1 in
+    -u|--user)
+      user_services=true
+      shift
+      ;;
+    -*)
+      softfail "Unknown argument: $1" || return $?
+      ;;
+    *)
+      break
+      ;;
+    esac
+  done
+
+  systemd-analyze ${user_services:+"--user"} verify "${1}.service" || fail
+  systemctl ${user_services:+"--user"} --quiet reenable "${1}.timer" || fail
+  systemctl ${user_services:+"--user"} start "${1}.timer" || fail
 }
 
 systemd::show_status() {
-  local systemctl_args=()
+  local user_services
   local with_timer=false
 
   while [ "$#" -gt 0 ]; do
     case $1 in
     -u|--user)
-      systemctl_args+=("--user")
+      user_services=true
       shift
       ;;
     -t|--with-timer)
@@ -161,16 +172,16 @@ systemd::show_status() {
   printf "\n"
 
   if [ "${with_timer}" = true ]; then
-    systemctl "${systemctl_args[@]}" list-timers "${1}.timer" --all || softfail || return $?
+    systemctl ${user_services:+"--user"} list-timers "${1}.timer" --all || softfail || return $?
     exit_statuses+=($?)
     printf "\n\n\n"
 
-    systemctl "${systemctl_args[@]}" status "${1}.timer"
+    systemctl ${user_services:+"--user"} status "${1}.timer"
     exit_statuses+=($?)
     printf "\n\n\n"
   fi
 
-  systemctl "${systemctl_args[@]}" status "${1}.service"
+  systemctl ${user_services:+"--user"} status "${1}.service"
   exit_statuses+=($?)
   printf "\n"
 
