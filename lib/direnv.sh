4adb6ae5bb8103412dead33eddb40a61cda80e7c
Stanislav Senotrusov <senotrusov@gmail.com>
2024-10-09 21:07:35 +0200

    direnv check
    Signed-off-by: Stanislav Senotrusov <senotrusov@gmail.com>

--- lib/direnv.sh
+++ lib/direnv.sh
@@ -42,21 +42,36 @@ direnv::save_variable_block() (
     esac
   done
 
-  # what is the difference between `loadedRC` and `foundRC` in `direnv status --json` output? Did I pick the right one for use below?
-
   local envrc_dir; envrc_dir="$(dirname "${envrc_path}")" || softfail || return $?
   local envrc_basename; envrc_basename="$(basename "${envrc_path}")" || softfail || return $?
 
   cd "${envrc_dir}" || softfail "Unable to change directory: ${envrc_dir}" || return $?
 
   if [ "${skip_allow_check}" != true ]; then
-    local found_any; found_any="$(direnv status --json | jq --raw-output --exit-status 'if (.state | has("loadedRC")) and (.state | has("foundRC")) then if .state.loadedRC == null and .state.foundRC == null then "empty-ok" else "non-empty" end else false end'; test "${PIPESTATUS[*]}" = "0 0")" || softfail "Unable to obtain empty list from direnv status" || return $?
+    local json_support_test; json_support_test="$(direnv status --json | head -c 1; test "${PIPESTATUS[*]}" = "0 0")" || softfail "Unable to obtain direnv status" || return $?
 
-    if [ "${found_any}" != "empty-ok" ]; then
-      local allowed_path; allowed_path="$(direnv status --json | jq --raw-output --exit-status 'if .state.loadedRC.allowed == 0 then .state.loadedRC.path else "" end'; test "${PIPESTATUS[*]}" = "0 0")" || softfail "Unable to obtain loadedRC.path from direnv status" || return $?
+    if [ "${json_support_test}" = "{" ]; then
+      local found_any; found_any="$(direnv status --json | jq --raw-output --exit-status 'if (.state | has("foundRC")) and (.state | has("foundRC")) then if .state.foundRC == null and .state.foundRC == null then "empty-ok" else "non-empty" end else false end'; test "${PIPESTATUS[*]}" = "0 0")" || softfail "Unable to obtain possible empty list from direnv status" || return $?
 
-      if [ "${allowed_path}" != "${PWD}/${envrc_basename}" ]; then
-        softfail "Found envrc file that is not currently allowed, or allowed file is different than provided: ${PWD}/${envrc_basename}"
+      if [ "${found_any}" != "empty-ok" ]; then
+        local allowed_path
+
+        if ! allowed_path="$(direnv status --json | jq --raw-output --exit-status 'if .state.foundRC.allowed == 0 then .state.foundRC.path else false end'; test "${PIPESTATUS[*]}" = "0 0")"; then
+          direnv status --json >&2
+          softfail "Found envrc file that is not currently allowed or unable to obtain foundRC.path from direnv status"
+          return $?
+        fi
+
+        if [ "${allowed_path}" != "${PWD}/${envrc_basename}" ]; then
+          direnv status --json >&2
+          softfail "Allowed envrc file path is different than provided: ${PWD}/${envrc_basename}"
+          return $?
+        fi
+      fi
+    else
+      if ! { direnv status | grep -qFx "No .envrc or .env found" || { direnv status | grep -qFx "Found RC path ${PWD}/${envrc_basename}" && { direnv status | grep -qFx "Found RC allowed 0" || direnv status | grep -qFx "Found RC allowed true"; }; }; }; then
+        direnv status >&2
+        softfail "Found envrc file that is not currently allowed, or allowed file path is different than provided: ${PWD}/${envrc_basename}"
         return $?
       fi
     fi
