4201f6c21bd1ddc1dfc74b61221244b94bf799d9
Stanislav Senotrusov <senotrusov@gmail.com>
2025-05-12 03:10:26 +0200

    Add comments and usage documentation for cross_platform::config_home
    Signed-off-by: Stanislav Senotrusov <senotrusov@gmail.com>

--- lib/cross_platform.sh
+++ lib/cross_platform.sh
@@ -14,19 +14,37 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# ## `cross_platform::config_home`
+#
+# Determines the platform-specific configuration home directory.
+#
+# This function checks the `OSTYPE` environment variable to identify the
+# operating system and then outputs the conventional path for user-specific
+# application configuration files. It supports Linux, macOS (darwin), and
+# Windows (msys).
+#
+# ### Usage
+#
+# cross_platform::config_home
+#
 cross_platform::config_home() {
+  # Determine configuration directory based on the operating system type
   case "${OSTYPE}" in
     linux*)
+      # For Linux, use XDG_CONFIG_HOME or default to ~/.config
       echo "${XDG_CONFIG_HOME:-"${HOME}/.config"}"
       ;;
     darwin*)
+      # For macOS, use ~/Library/Application Support
       echo "${HOME}/Library/Application Support"
-      ;; 
+      ;;
     msys*)
+      # For Windows (msys/git bash), use APPDATA
       echo "${APPDATA}"
       ;;
     *)
-      softfail "Platform is not supported" || return $?
+      # If the platform is not recognized, report a soft failure
+      softfail "The operating system platform is not supported." || return $?
       ;;
   esac
 }
